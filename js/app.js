5028e2f826d79b014e039bc7fc761ee80f29e266
Beau Hibbert <beauhibbert789@gmail.com>
2021-10-26 22:17:53 -0700

    fixed syntax

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ let college =prompt("Did I go to WSU for college?").toLowerCase();
 console.log("college: ", college)
 
 let dog =prompt("Do I have a dog").toLowerCase();
-console.log("dog: ". dog)
+console.log("dog: ", dog)
 
 let soda =prompt("Is Diet Mt Dew my favorite soda?").toLowerCase();
-console.log("soda: ". soda)
+console.log("soda: ", soda)
