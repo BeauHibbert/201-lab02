82b85a905e7189915473e2e017c136f3c8036ac2
Beau Hibbert <beauhibbert789@gmail.com>
2021-10-26 22:06:44 -0700

    linked js and html

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
-let origin = prompt("Am I from Seattle?");
+let origin = prompt("Am I from Seattle?").toLowerCase();
+console.log("origin: ", origin)
 
-let color = prompt("Is my favorite color blue?");
+let color = prompt("Is my favorite color blue?").toLowerCase();
 
-let college =prompt("Did I go to WSU for college?");
+let college =prompt("Did I go to WSU for college?").toLowerCase();
 
-let dog =prompt("Do I have a dog");
+let dog =prompt("Do I have a dog").toLowerCase();
 
-let soda =prompt("Is Diet Mt Dew my favorite soda?");
+let soda =prompt("Is Diet Mt Dew my favorite soda?").toLowerCase();
