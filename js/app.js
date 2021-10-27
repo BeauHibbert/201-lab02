ef9d0a5cfe095910ad807c10db15f7643f4e2035
Beau Hibbert <beauhibbert789@gmail.com>
2021-10-26 21:47:27 -0700

    added js prompts

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let origin = prompt("Am I from Seattle?");
+
+let color = prompt("Is my favorite color blue?");
+
+let college =prompt("Did I go to WSU for college?");
+
+let dog =prompt("Do I have a dog");
+
+let soda =prompt("Is Diet Mt Dew my favorite soda?");
