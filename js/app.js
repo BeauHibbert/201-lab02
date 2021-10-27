26567aee365d34849f15e6ffcb93427e02fafd84
Beau Hibbert <beauhibbert789@gmail.com>
2021-10-26 22:15:46 -0700

    added console.logs

--- js/app.js
+++ js/app.js
@@ -2,9 +2,13 @@ let origin = prompt("Am I from Seattle?").toLowerCase();
 console.log("origin: ", origin)
 
 let color = prompt("Is my favorite color blue?").toLowerCase();
+console.log("color: ", color)
 
 let college =prompt("Did I go to WSU for college?").toLowerCase();
+console.log("college: ", college)
 
 let dog =prompt("Do I have a dog").toLowerCase();
+console.log("dog: ". dog)
 
 let soda =prompt("Is Diet Mt Dew my favorite soda?").toLowerCase();
+console.log("soda: ". soda)
