8941e732361004426601040c696fc401498944c5
Beau Hibbert <beauhibbert789@gmail.com>
2021-10-28 00:00:07 -0700

    half of lab 3

--- js/app.js
+++ js/app.js
@@ -49,30 +49,28 @@ if (soda === "yes" || soda === "y") {
     alert("Sorry, that is wrong");
 }
 
-
-let number =prompt("I'm thinking of a number between 1 and 100. Can you guess it??")
-let attempts = 0
-let correctAnswer = 68
-
-while(attempts<=4) {
-  if (number===correctAnswer) {
-      alert("Congrats, thats correct!")
-      break
-  } else {
-      alert("Sorry, try again.")
-      attempts++
+function numberGuessingGame() {
+  let userGuess = parseInt(prompt("I'm thinking of a number between 1 and 100. Can you guess it??"));
+  let attempts = 0;
+  let correctAnswer = 68;
+  
+  while ((userGuess !== correctAnswer) && (attempts < 3)){
+    if(userGuess<correctAnswer) {
+      userGuess = prompt("That number is too low of a guess, try again.");
+      attempts++;
+    } else if(userGuess>correctAnswer) {
+      userGuess = prompt("That number is too high, guess again.");
+      attempts++;
+    }
   }
+
+  if (userGuess === correctAnswer) {
+    alert('You got it right!')
+  }
+
+  if (userGuess !== correctAnswer && attempts === 3) {
+    alert("Sorry, out of guesses. The correct answer was 68.")
+  } 
 }
 
-
-
-
-// for(let i = 0; i <= 4; i++) {
-//     if (number===correctAnswer) {
-//         alert("Congrats, thats correct!")
-//         break
-//     } else {
-//         alert("Sorry, try again.")
-//         i++
-//     }
-// }
+numberGuessingGame();
